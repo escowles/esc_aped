17a1a1eb701070c46c3395d6edf4de43496aea7b
Esmé Cowles <escowles@ucsd.edu>
2014-05-26 10:57:44 -0400

    bring config into esc_aped.rb, combining irc/twitter logic

--- esc_aped.rb
+++ esc_aped.rb
@@ -1,56 +1,17 @@
 #!/usr/bin/env ruby
 
-require 'config.rb'
+# esc_aped - an irc/twitter bridge
+# 
+# listens to irc channel(s) and forwards messages to twitter, keeps track of
+# messages so twitter replies are turned into irc replies
+
+require 'yaml'
 require 'cinch'
 require 'thread'
 require 'twitter'
 
-# sent a tweet
-# from_nick: irc nick who initiated the message
-# recipient: twitter nick of the user to tweet at
-# text: tweet text
-def twitter_send( from_nick, recipient, text )
-  unless @twitter_send do
-    @twitter_send = Twitter::REST::Client.new do |cfg|
-      cfg.consumer_key = @config['twitter']['api_key']
-      cfg.consumer_secret = @config['twitter']['api_secret']
-      cfg.access_token = @config['twitter']['access_token']
-      cfg.access_token_secret = @config['twitter']['access_secret']
-    end
-  end
-  tweet = @twitter_send.update("@#{recipient}: #{text}")
-  @tweets_sent[tweet.id] = from_nick
-end
-
-# listen to tweets and forward replies to irc
-def twitter_listen
-  unless @twitter_receive do
-    @twitter_receive = Twitter::Streaming::Client.new do |cfg|
-      cfg.consumer_key = @config['twitter']['api_key']
-      cfg.consumer_secret = @config['twitter']['api_secret']
-      cfg.access_token = @config['twitter']['access_token']
-      cfg.access_token_secret = @config['twitter']['access_secret']
-    end
-  end
-  @twitter_receive.user do |obj|
-    case obj
-    when Twitter::Tweet
-      from_nick = @tweets_sent[obj.in_reply_to_status_id]
-      irc_send( from_nick, obj.text )
-    end
-  end
-end
-
-# send an irc message
-# recipient: irc nick of the user to mention
-# text: message text
-def irc_send( recipient, text )
-  irc_listen # setup @irc connection
-  @irc.send( "#{recipient}: #{text}" )
-end
-
 # listen to irc messages and forward mentions to twitter
-def irc_listen
+def irc_to_twitter
   unless @irc
     @irc = Cinch::Bot.new do
       configure do |cfg|
@@ -62,24 +23,56 @@ def irc_listen
       end
     end
 
-    on :message, /esc_aped/ do |msg|
-      text = "*#{msg.action_message}" if msg.action_message
-      text ||= msg.message
-      venue = msg.channel || "@#{msg.target}"
-      if msg.message =~ /^esc_aped/
-        msg.reply "you rang?"
-      else
-        msg.reply "yes?"
+    on :message, /"#{nick}"/ do |msg|
+      unless @twitter_send do
+        @twitter_send = Twitter::REST::Client.new do |cfg|
+          cfg.consumer_key = @config['twitter']['api_key']
+          cfg.consumer_secret = @config['twitter']['api_secret']
+          cfg.access_token = @config['twitter']['access_token']
+          cfg.access_token_secret = @config['twitter']['access_secret']
+        end
       end
 
-      puts "\n#{msg.time} #{venue} #{msg.user} #{text}\n\n"
-      recipient = "escowles" # XXX
-      twitter_send( msg.user, recipient, text )
+      mention = @config['twitter']['mention']
+      text = "@#{mention}: " if mention
+      if msg.action_message 
+        text += "*#{msg.action_message}"
+      else
+        text += msg.message
+      end
+      # XXX: strip nick: prefix...
+
+      tweet = @twitter_send.update(text)
+      @tweets_sent[tweet.id] = msg
     end
   end
 end
 
+# listen to tweets and forward replies to irc
+def twitter_to_irc
+  unless @twitter_receive do
+    @twitter_receive = Twitter::Streaming::Client.new do |cfg|
+      cfg.consumer_key = @config['twitter']['api_key']
+      cfg.consumer_secret = @config['twitter']['api_secret']
+      cfg.access_token = @config['twitter']['access_token']
+      cfg.access_token_secret = @config['twitter']['access_secret']
+    end
+  end
+  @twitter_receive.user do |obj|
+    case obj
+    when Twitter::Tweet
+      msg = @tweets_sent[obj.in_reply_to_status_id]
+      msg.reply( obj.text )
+    end
+  end
+end
+
+
+# load config
+@config = YAML.load_file('config.yml')
+
+# start irc_to_twitter and twitter_to_irc
 threads = []
-threads[0] = Thread.new { irc_listen }
-threads[1] = Thread.new { twitter_listen }
+threads[0] = Thread.new { irc_to_twitter }
+threads[1] = Thread.new { twitter_to_irc }
 threads.each { |t| t.join } # wait for all threads to finish
